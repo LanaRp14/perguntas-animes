ee4aad0bd37a8d5180f75927e3c72ccf8aec7383
LanaRp14 <135618286+LanaRp14@users.noreply.github.com>
2024-10-21 12:31:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'One piece',
+    'Quem foi oprimeiro membro a entrar na tripulação do Luffy?',
+    'Ronoroa Zoro'
+
+)
+
+criaCartao(
+    'One piece',
+    'Quem matou o irmão de luffy, Portgas D. Ace?',
+    'Almirante Akainu'
+
+)
+
+criaCartao(
+    'One piece',
+    'Quem é Zunesha?',
+    'É um elefante milenar que carrega a ilha Zou nas costas e foi condenado a vagar pelos mare por crime misterioso e pode se comunicar com Luffy e Momonouke'
+)
+
+criaCartao(
+    'One piece',
+    'Quantos Poneglyphs exitem no mundo de One Piece?',
+    'Exitem 30 Poneglyphs conhecido, sendo 4 deles os Road Poneglyphs'
+)
